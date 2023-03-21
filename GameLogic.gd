a3d02cd6959eaf0cf76ae209cddbad3eaed473fa
Timothy Stiles <Patashu@hotmail.com>
2023-03-21 23:25:09 +1100

    Firewall [VAR1]

--- GameLogic.gd
+++ GameLogic.gd
@@ -209,6 +209,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/SnakePitEx2.tscn"));
 	level_list.push_back(preload("res://levels/Acrobatics.tscn"));
 	level_list.push_back(preload("res://levels/Firewall.tscn"));
+	level_list.push_back(preload("res://levels/FirewallEx.tscn"));
 	level_list.push_back(preload("res://levels/UnderDestination.tscn"));
 	level_list.push_back(preload("res://levels/UnderDestinationEx.tscn"));
 	level_list.push_back(preload("res://levels/TrophyCabinet.tscn"));
