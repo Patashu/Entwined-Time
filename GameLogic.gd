7ded01a4632eec8aad11800bdb49604fbeb2965a
Timothy Stiles <Patashu@hotmail.com>
2023-03-21 22:24:04 +1100

    Over-Destination (and [VAR1])

--- GameLogic.gd
+++ GameLogic.gd
@@ -211,6 +211,8 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/Firewall.tscn"));
 	level_list.push_back(preload("res://levels/UnderDestination.tscn"));
 	level_list.push_back(preload("res://levels/UnderDestinationEx.tscn"));
+	level_list.push_back(preload("res://levels/OverDestination.tscn"));
+	level_list.push_back(preload("res://levels/OverDestinationEx.tscn"));
 	level_list.push_back(preload("res://levels/TrophyCabinet.tscn"));
 	level_list.push_back(preload("res://levels/SteppingStool.tscn"));
 	level_list.push_back(preload("res://levels/SteppingStoolEx.tscn"));
