f95638d5389d6f742db3c33014a22d40c156c137
Timothy Stiles <Patashu@hotmail.com>
2023-04-09 17:45:09 +1000

    Roommates [VAR1]

--- GameLogic.gd
+++ GameLogic.gd
@@ -224,6 +224,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/Spelunking.tscn"));
 	level_list.push_back(preload("res://levels/TheFirstPitEx.tscn"));
 	level_list.push_back(preload("res://levels/TheFirstPitEx2.tscn"));
+	level_list.push_back(preload("res://levels/RoommatesEx.tscn"));
 	level_list.push_back(preload("res://levels/Graduation.tscn"));
 	
 	chapter_names.push_back("Hazards");
