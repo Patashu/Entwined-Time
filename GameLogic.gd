673c5e6bd211376798c18d15fb3898d13aaa260c
Timothy Stiles <Patashu@hotmail.com>
2023-04-09 17:55:32 +1000

    Uphill

--- GameLogic.gd
+++ GameLogic.gd
@@ -219,6 +219,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/Roommates.tscn"));
 	level_list.push_back(preload("res://levels/UncabYourself.tscn"));
 	level_list.push_back(preload("res://levels/Downhill.tscn"));
+	level_list.push_back(preload("res://levels/Uphill.tscn"));
 	
 	chapter_advanced_starting_levels.push_back(level_list.size());
 	level_list.push_back(preload("res://levels/Spelunking.tscn"));
