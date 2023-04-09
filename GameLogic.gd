98c99f1a701c552521a6758dba475cbae20f715c
Timothy Stiles <Patashu@hotmail.com>
2023-04-09 21:27:23 +1000

    Under-Destination de-cheese

--- GameLogic.gd
+++ GameLogic.gd
@@ -234,7 +234,6 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/SnakePit.tscn"));
 	level_list.push_back(preload("res://levels/Firewall.tscn"));
 	level_list.push_back(preload("res://levels/UnderDestination.tscn"));
-	level_list.push_back(preload("res://levels/UnderDestinationEx.tscn"));
 	level_list.push_back(preload("res://levels/No.tscn"));
 	level_list.push_back(preload("res://levels/Acrobatics.tscn"));
 	
@@ -243,6 +242,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/SnakePitEx2.tscn"));
 	level_list.push_back(preload("res://levels/FirewallEx.tscn"));
 	level_list.push_back(preload("res://levels/FirewallEx2.tscn"));
+	level_list.push_back(preload("res://levels/UnderDestinationEx.tscn"));
 	level_list.push_back(preload("res://levels/AcrobaticsEx.tscn"));
 	level_list.push_back(preload("res://levels/OrbitalDrop.tscn"));
 	
