fc14bcd030cf02aaf7210fb98a5d2c7d4759610b
Timothy Stiles <Patashu@hotmail.com>
2023-04-11 09:54:06 +1000

    Security Door [VAR1] de-cheese split

--- GameLogic.gd
+++ GameLogic.gd
@@ -260,6 +260,7 @@ func initialize_level_list() -> void:
 	
 	chapter_advanced_starting_levels.push_back(level_list.size());
 	level_list.push_back(preload("res://levels/SecurityDoorEx.tscn"));
+	level_list.push_back(preload("res://levels/SecurityDoorEx2.tscn"));
 	level_list.push_back(preload("res://levels/JailEx.tscn"));
 	level_list.push_back(preload("res://levels/JailEx2.tscn"));
 	level_list.push_back(preload("res://levels/HawkingRadiation.tscn"));
