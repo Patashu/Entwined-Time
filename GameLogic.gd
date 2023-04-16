c047302a0f0eed7295e5d7087feb92246b35ef46
Timothy Stiles <Patashu@hotmail.com>
2023-04-16 14:03:53 +1000

    Hell [VAR1]

--- GameLogic.gd
+++ GameLogic.gd
@@ -267,6 +267,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/SnakePitEx2.tscn"));
 	level_list.push_back(preload("res://levels/FirewallEx.tscn"));
 	level_list.push_back(preload("res://levels/FirewallEx2.tscn"));
+	level_list.push_back(preload("res://levels/HellEx.tscn"));
 	level_list.push_back(preload("res://levels/UnderDestination.tscn"));
 	level_list.push_back(preload("res://levels/UnderDestinationEx.tscn"));
 	level_list.push_back(preload("res://levels/OrbitalDrop.tscn"));
