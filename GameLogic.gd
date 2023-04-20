d63e5cbb062acbb1e2c0136c16fba67587674eeb
Timothy Stiles <Patashu@hotmail.com>
2023-04-20 21:48:10 +1000

    nerfed Heavy Moving Service, and Light Moving Service

--- GameLogic.gd
+++ GameLogic.gd
@@ -318,6 +318,7 @@ func initialize_level_list() -> void:
 	chapter_standard_starting_levels.push_back(level_list.size());
 	chapter_skies.push_back(Color("#062138"));
 	level_list.push_back(preload("res://levels/HeavyMovingService.tscn"));
+	level_list.push_back(preload("res://levels/LightMovingService.tscn"));
 	level_list.push_back(preload("res://levels/InvisibleBridge.tscn"));
 	level_list.push_back(preload("res://levels/Acrobatics.tscn"));
 	level_list.push_back(preload("res://levels/AcrobaticsEx.tscn"));
