5bd9c1fbf8572600590d470d42883051708a3893
Timothy Stiles <Patashu@hotmail.com>
2023-04-28 19:57:24 +1000

    Diving Board de-cheese

--- GameLogic.gd
+++ GameLogic.gd
@@ -443,6 +443,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/BlueAndRedEx2.tscn"));
 	level_list.push_back(preload("res://levels/PaperPlanesEx.tscn"));
 	level_list.push_back(preload("res://levels/TimelessBridgeEx.tscn"));
+	level_list.push_back(preload("res://levels/LevitationColours.tscn"));
 	level_list.push_back(preload("res://levels/Towerplex.tscn"));
 	
 	# sentinel to make overflow checks easy
