39381b8802baf204150efea38b3b20ae53860881
Timothy Stiles <Patashu@hotmail.com>
2023-04-28 17:05:35 +1000

    The Red Pit

--- GameLogic.gd
+++ GameLogic.gd
@@ -428,6 +428,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/DownhillRedBlue.tscn"));
 	level_list.push_back(preload("res://levels/SpelunkingRedBlue.tscn"));
 	level_list.push_back(preload("res://levels/BlueAndRed.tscn"));
+	level_list.push_back(preload("res://levels/TheRedPit.tscn"));
 	level_list.push_back(preload("res://levels/PaperPlanes.tscn"));
 	level_list.push_back(preload("res://levels/TimelessBridge.tscn"));
 	
