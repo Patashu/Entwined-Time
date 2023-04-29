304bbe751823594043da903cbe260590c9b3917b
Timothy Stiles <Patashu@hotmail.com>
2023-04-29 20:09:03 +1000

    Interleave

--- GameLogic.gd
+++ GameLogic.gd
@@ -463,6 +463,7 @@ func initialize_level_list() -> void:
 	chapter_skies.push_back(Color("#446570"));
 	level_list.push_back(preload("res://levels/Ahhh.tscn"));
 	level_list.push_back(preload("res://levels/Eeep.tscn"));
+	level_list.push_back(preload("res://levels/Interleave.tscn"));
 	
 	chapter_advanced_starting_levels.push_back(level_list.size());
 	
