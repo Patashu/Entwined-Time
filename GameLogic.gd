252f4d375262164da2215d3a8b3519310be5618b
Timothy Stiles <Patashu@hotmail.com>
2023-05-01 20:41:53 +1000

    Demolition Squad and [VAR1] solutions

--- GameLogic.gd
+++ GameLogic.gd
@@ -478,9 +478,10 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/TreasureHunt.tscn"));
 	
 	chapter_advanced_starting_levels.push_back(level_list.size());
-	level_list.push_back(preload("res://levels/DoubleGlazedEx.tscn"));
 	level_list.push_back(preload("res://levels/LetMeInEx.tscn"));
 	level_list.push_back(preload("res://levels/HeavyMovingServiceGlass.tscn"));
+	level_list.push_back(preload("res://levels/DoubleGlazedEx.tscn"));
+	level_list.push_back(preload("res://levels/DemolitionSquadEx.tscn"));
 	level_list.push_back(preload("res://levels/SpelunkingGlassEx.tscn"));
 	level_list.push_back(preload("res://levels/SpelunkingGlassEx2.tscn"));
 	level_list.push_back(preload("res://levels/TheGlassPitEx.tscn"));
