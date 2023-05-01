5cbb42c510693673750040f4b5e9cddefa6d7616
Timothy Stiles <Patashu@hotmail.com>
2023-05-01 20:54:08 +1000

    Campfrost and [VAR1] solutions

--- GameLogic.gd
+++ GameLogic.gd
@@ -481,10 +481,11 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/LetMeInEx.tscn"));
 	level_list.push_back(preload("res://levels/HeavyMovingServiceGlass.tscn"));
 	level_list.push_back(preload("res://levels/DoubleGlazedEx.tscn"));
-	level_list.push_back(preload("res://levels/DemolitionSquadEx.tscn"));
 	level_list.push_back(preload("res://levels/SpelunkingGlassEx.tscn"));
-	level_list.push_back(preload("res://levels/SpelunkingGlassEx2.tscn"));
+	level_list.push_back(preload("res://levels/DemolitionSquadEx.tscn"));
 	level_list.push_back(preload("res://levels/TheGlassPitEx.tscn"));
+	level_list.push_back(preload("res://levels/CampfireGlassEx.tscn"));
+	level_list.push_back(preload("res://levels/SpelunkingGlassEx2.tscn"));
 	
 	# sentinel to make overflow checks easy
 	chapter_standard_starting_levels.push_back(level_list.size());
