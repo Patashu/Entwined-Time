76113d2668593d9bc276fc62b218ca87282a5735
Timothy Stiles <Patashu@hotmail.com>
2023-05-03 22:23:30 +1000

    'Green Grass'

--- GameLogic.gd
+++ GameLogic.gd
@@ -519,6 +519,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/LightHurtingService.tscn"));
 	level_list.push_back(preload("res://levels/LightHurtingServiceEx.tscn"));
 	level_list.push_back(preload("res://levels/LightHurtingServiceEx2.tscn"));
+	level_list.push_back(preload("res://levels/GreenGrass.tscn"));
 	level_list.push_back(preload("res://levels/Skip.tscn"));
 	
 	# sentinel to make overflow checks easy
