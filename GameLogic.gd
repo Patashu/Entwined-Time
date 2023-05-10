f350ac2ff0502d5128ea82346467e705d2b3aa13
Timothy Stiles <Patashu@hotmail.com>
2023-05-10 11:20:36 +1000

    fix the loop 2 talls and walls

--- GameLogic.gd
+++ GameLogic.gd
@@ -573,6 +573,7 @@ func initialize_level_list() -> void:
 #	level_list.push_back(preload("res://levels/BraidL2.tscn"));
 	level_list.push_back(preload("res://levels/TallL2.tscn"));
 	level_list.push_back(preload("res://levels/TallL2Ex.tscn"));
+	level_list.push_back(preload("res://levels/TallL2Ex2.tscn"));
 	level_list.push_back(preload("res://levels/WallL2.tscn"));
 	level_list.push_back(preload("res://levels/WallL2Ex.tscn"));
 #	level_list.push_back(preload("res://levels/PushingItL2.tscn"));
