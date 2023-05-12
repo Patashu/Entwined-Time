a863780822716ac6abc8f23f0daf9f243f56de00
Timothy Stiles <Patashu@hotmail.com>
2023-05-12 22:58:09 +1000

    pushing it decheeses/buffs

--- GameLogic.gd
+++ GameLogic.gd
@@ -571,6 +571,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/KingCrimson.tscn"));
 	level_list.push_back(preload("res://levels/LimitedUndo.tscn"));
 	chapter_advanced_starting_levels.push_back(level_list.size());
+	level_list.push_back(preload("res://levels/PushingItFurtherEx.tscn"));
 	level_list.push_back(preload("res://levels/KingCrimsonEx.tscn"));
 	level_list.push_back(preload("res://levels/LimitedUndoEx.tscn"));
 	
