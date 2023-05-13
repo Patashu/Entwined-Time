6df991f2b1e50931d8b7ae2c64bd24cedc5f1928
Timothy Stiles <Patashu@hotmail.com>
2023-05-13 21:59:47 +1000

    Lead Balloon and solution

--- GameLogic.gd
+++ GameLogic.gd
@@ -569,6 +569,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/ImaginaryMoves.tscn"));
 	level_list.push_back(preload("res://levels/PushingItFurther.tscn"));
 	level_list.push_back(preload("res://levels/KingCrimson.tscn"));
+	level_list.push_back(preload("res://levels/LeadBalloon.tscn"));
 	level_list.push_back(preload("res://levels/DontLookDown.tscn"));
 	level_list.push_back(preload("res://levels/LimitedUndo.tscn"));
 	level_list.push_back(preload("res://levels/UnfathomableGlass.tscn"));
