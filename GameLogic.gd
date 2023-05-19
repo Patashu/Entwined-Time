55cdb4a75ac5d0f2a62224e4bb6b5f27383c322e
Timothy Stiles <Patashu@hotmail.com>
2023-05-19 21:00:56 +1000

    Forgetfulness and solution

--- GameLogic.gd
+++ GameLogic.gd
@@ -632,6 +632,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/Wither.tscn"));
 	level_list.push_back(preload("res://levels/Bounce.tscn"));
 	level_list.push_back(preload("res://levels/Pathology.tscn"));
+	level_list.push_back(preload("res://levels/Forgetfulness.tscn"));
 	level_list.push_back(preload("res://levels/Test.tscn"));
 	chapter_advanced_starting_levels.push_back(level_list.size());
 	level_list.push_back(preload("res://levels/BlockageEx.tscn"));
