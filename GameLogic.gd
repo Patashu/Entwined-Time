972e6d5ac7283674494816c04448d3fdb8199e95
Timothy Stiles <Patashu@hotmail.com>
2023-05-21 15:53:01 +1000

    retire Spelunking?, add Lunar Gravity

--- GameLogic.gd
+++ GameLogic.gd
@@ -527,7 +527,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/RedAndBlue.tscn"));
 	level_list.push_back(preload("res://levels/LevelNotFound.tscn"));
 	level_list.push_back(preload("res://levels/DownhillRedBlue.tscn"));
-	level_list.push_back(preload("res://levels/SpelunkingRedBlue.tscn"));
+	level_list.push_back(preload("res://levels/LunarGravity.tscn"));
 	level_list.push_back(preload("res://levels/BlueAndRed.tscn"));
 	level_list.push_back(preload("res://levels/TheRedPit.tscn"));
 	level_list.push_back(preload("res://levels/TheBluePit.tscn"));
