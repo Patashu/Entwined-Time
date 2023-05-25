b1e320c7fb0dac28a844886725cd2c15cc70ab84
Timothy Stiles <Patashu@hotmail.com>
2023-05-25 22:01:40 +1000

    prototype Rewind

--- GameLogic.gd
+++ GameLogic.gd
@@ -681,6 +681,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/DST.tscn"));
 	level_list.push_back(preload("res://levels/EngineRoom.tscn"));
 	level_list.push_back(preload("res://levels/TheShroud.tscn"));
+	level_list.push_back(preload("res://levels/Rewind.tscn"));
 	level_list.push_back(preload("res://levels/ControlledDemolition.tscn"));
 	level_list.push_back(preload("res://levels/Cascade.tscn"));
 	chapter_advanced_starting_levels.push_back(level_list.size());
