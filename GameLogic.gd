6e2803106dff719bb8582d4070292053dc50f07c
Timothy Stiles <Patashu@hotmail.com>
2023-06-06 20:16:22 +1000

    Pushing It 2 and solution

--- GameLogic.gd
+++ GameLogic.gd
@@ -546,6 +546,7 @@ func initialize_level_list() -> void:
 	level_list.push_back(preload("res://levels/Downstream.tscn"));
 	level_list.push_back(preload("res://levels/TheOneWayPit.tscn"));
 	level_list.push_back(preload("res://levels/EventHorizon.tscn"));
+	level_list.push_back(preload("res://levels/PushingItSequel.tscn"));
 	
 	chapter_advanced_starting_levels.push_back(level_list.size());
 	chapter_advanced_unlock_requirements.push_back(24);
