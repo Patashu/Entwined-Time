f5d96aa5cdf47bfeee3da7fecfc22bf229326670
Timothy Stiles <Patashu@hotmail.com>
2023-06-09 17:19:17 +1000

    Weakness [VAR1] and solution

--- GameLogic.gd
+++ GameLogic.gd
@@ -615,6 +615,7 @@ func initialize_level_list() -> void:
 	chapter_advanced_starting_levels.push_back(level_list.size());
 	chapter_advanced_unlock_requirements.push_back(40);
 	level_list.push_back(preload("res://levels/Levitation.tscn"));
+	level_list.push_back(preload("res://levels/WeaknessEx.tscn"));
 	level_list.push_back(preload("res://levels/SteppingStoolEx.tscn"));
 	level_list.push_back(preload("res://levels/OverDestinationEx.tscn"));
 	level_list.push_back(preload("res://levels/ThirdRoommateEx.tscn"));
