0dd436c4ca21145a708df1a94e75f86fbba1ef8d
Timothy Stiles <Patashu@hotmail.com>
2023-06-18 21:53:27 +1000

    remove 'void cuckoo clocks tick in real time' mechanic
    A Way In? should be a better puzzle if it's a green cuckoo clock instead.

--- GameLogic.gd
+++ GameLogic.gd
@@ -257,9 +257,6 @@ var heavy_actor : Actor = null
 var light_actor : Actor = null
 var actors = []
 var goals = []
-var void_cuckoo_clocks = [];
-var void_cuckoo_clock_timer = 0;
-var void_cuckoo_clock_timer_max = 1;
 var heavy_turn = 0;
 var heavy_undo_buffer : Array = [];
 var heavy_filling_locked_turn_index = -1;
@@ -881,8 +878,6 @@ func ready_map() -> void:
 	for ghost in ghosts:
 		ghost.queue_free();
 	ghosts.clear();
-	void_cuckoo_clocks.clear();
-	void_cuckoo_clock_timer = 0;
 	for whatever in underactorsparticles.get_children():
 		whatever.queue_free();
 	for whatever in overactorsparticles.get_children():
@@ -1254,8 +1249,6 @@ func find_colour(id: int, time_colour : int) -> void:
 			for actor in actors:
 				if actor.pos == tile and actor.is_native_colour():
 					actor.time_colour = time_colour;
-					if (actor.time_colour == TimeColour.Void and actor.actorname == Actor.Name.CuckooClock):
-						void_cuckoo_clocks.append(actor);
 					actor.update_time_bubble();
 					break;
 	
@@ -3506,13 +3499,6 @@ func time_passes(chrono: int) -> void:
 		if actor.ticks < 10000 and !actor.broken:
 			clock_ticks(actor, -1, chrono);
 	
-func void_cuckoo_clocks_time_passes() -> void:
-	for actor in void_cuckoo_clocks:
-		if actor.in_night:
-			continue;
-		if actor.ticks < 10000 and !actor.broken:
-			clock_ticks(actor, -1, Chrono.META_UNDO);
-	
 func bottom_up(a, b) -> bool:
 	# TODO: make this tiebreak by x, then by layer or id, so I can use it as a stable sort in general?
 	return a.pos.y > b.pos.y;
@@ -3890,13 +3876,6 @@ func _process(delta: float) -> void:
 		else:
 			Static.visible = false;
 			Static.modulate = Color(1, 1, 1, 1);
-	
-	if ui_stack.size() == 0:
-		if (void_cuckoo_clocks.size() > 0 and !won and !lost):
-			void_cuckoo_clock_timer += delta;
-			if (void_cuckoo_clock_timer > void_cuckoo_clock_timer_max):
-				void_cuckoo_clock_timer -= void_cuckoo_clock_timer_max;
-				void_cuckoo_clocks_time_passes();
 		
 		if (doing_replay and replay_timer > next_replay):
 			do_one_replay_turn();
