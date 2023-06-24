47812587bea4190f67cedb6b4110be0014999ce6
Timothy Stiles <Patashu@hotmail.com>
2023-06-24 23:39:10 +1000

    The Race and solution

--- GameLogic.gd
+++ GameLogic.gd
@@ -695,9 +695,9 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("LadderWorldGlassEx0.5")
 	level_filenames.push_back("SpelunkingGlassEx")
 	level_filenames.push_back("DemolitionSquadEx")
-	
 	level_filenames.push_back("Deconstruct")
 	level_filenames.push_back("TheGlassPitEx")
+	level_filenames.push_back("TheRace")
 	level_filenames.push_back("CampfireGlass")
 	level_filenames.push_back("CampfireGlassEx")
 	level_filenames.push_back("SpelunkingGlassEx2")
