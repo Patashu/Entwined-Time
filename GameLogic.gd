9e56259d0dd5a3c14aebc7eae93e1e1c0f9260ba
Timothy Stiles <Patashu@hotmail.com>
2023-06-26 19:57:01 +1000

    Victory Lap only needs 256 (or all previous) puzzles solved

--- GameLogic.gd
+++ GameLogic.gd
@@ -831,7 +831,7 @@ func initialize_level_list() -> void:
 	
 	chapter_names.push_back("Victory Lap");
 	chapter_standard_starting_levels.push_back(level_filenames.size());
-	chapter_standard_unlock_requirements.push_back(level_filenames.size());
+	chapter_standard_unlock_requirements.push_back(min(256, level_filenames.size()));
 	chapter_skies.push_back(Color("#223C52"));
 	chapter_replacements[chapter_names.size() - 1] = "-1";
 	level_filenames.push_back("RoommatesExL2")
