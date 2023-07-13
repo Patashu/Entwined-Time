21dc281fa0550ecb98e35b007ccf78c4672d0e51
Timothy Stiles <Patashu@hotmail.com>
2023-07-13 13:34:45 +1000

    The Gray Pit [VAR1] and solution

--- GameLogic.gd
+++ GameLogic.gd
@@ -923,6 +923,7 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("LevitationColours")
 	level_filenames.push_back("Towerplex")
 	level_filenames.push_back("TheMagentaPitEx")
+	level_filenames.push_back("TheGrayPitEx")
 	level_filenames.push_back("TheMagentaPitEx2")
 	
 	chapter_names.push_back("Change");
