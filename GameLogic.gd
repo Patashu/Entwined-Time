3fdcbcf339bef08c563f0dbb6ade602199c89abd
Timothy Stiles <Patashu@hotmail.com>
2023-07-13 13:11:25 +1000

    The Magenta Pit [VAR1] buff/nerf split

--- GameLogic.gd
+++ GameLogic.gd
@@ -923,6 +923,7 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("LevitationColours")
 	level_filenames.push_back("Towerplex")
 	level_filenames.push_back("TheMagentaPitEx")
+	level_filenames.push_back("TheMagentaPitEx2")
 	
 	chapter_names.push_back("Change");
 	chapter_standard_starting_levels.push_back(level_filenames.size());
