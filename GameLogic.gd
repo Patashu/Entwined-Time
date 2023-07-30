f9f5b59e508151dd7cab74d9d8c80bc29cd04d81
Timothy Stiles <Patashu@hotmail.com>
2023-07-30 21:49:06 +1000

    move Help Yourself [VAR1] even earlier

--- GameLogic.gd
+++ GameLogic.gd
@@ -1066,11 +1066,11 @@ func initialize_level_list() -> void:
 	
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(64);
+	level_filenames.push_back("HelpYourselfEx")
 	level_filenames.push_back("LightHurtingService")
 	level_filenames.push_back("LightHurtingServiceEx")
 	level_filenames.push_back("LightHurtingServiceEx2")
 	level_filenames.push_back("GreenGrass")
-	level_filenames.push_back("HelpYourselfEx")
 	level_filenames.push_back("SpikesGreenEx")
 	level_filenames.push_back("CampfireGreenEx")
 	level_filenames.push_back("FirewallGreenEx")
