57429ef60817e34bce4c95fde52259ef0316210c
Timothy Stiles <Patashu@hotmail.com>
2023-10-05 22:49:16 +1100

    adjusting fanfare volume slider plays fanfare

--- Settings.gd
+++ Settings.gd
@@ -113,7 +113,7 @@ func _fanfareslider_value_changed(value: float) -> void:
 	
 	gamelogic.save_file["fanfare_volume"] = value;
 	gamelogic.setup_volume();
-	gamelogic.play_sound("switch");
+	gamelogic.play_won("winentwined");
 	updatelabelfanfare(value);
 		
 func _musicslider_value_changed(value: float) -> void:
