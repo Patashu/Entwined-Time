b3fc64a1feb195d29cda52292bff89b80056f246
Timothy Stiles <Patashu@hotmail.com>
2024-02-14 20:25:58 +1100

    bring Roommates [VAR1] forward as it's easy

--- GameLogic.gd
+++ GameLogic.gd
@@ -968,10 +968,10 @@ func initialize_level_list() -> void:
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(8);
 	level_filenames.push_back("Spelunking")
+	level_filenames.push_back("RoommatesEx")
 	level_filenames.push_back("TheFirstPitEx")
 	level_filenames.push_back("TheFirstPitEx2")
 	level_filenames.push_back("CarryingItEx")
-	level_filenames.push_back("RoommatesEx")
 	level_filenames.push_back("BraidEx")
 	level_filenames.push_back("ShouldveCalledaCab")
 	level_filenames.push_back("UncabYourself")
