a8156b9616d4281fdf02bba6449802b8e3abfb61
Timothy Stiles <Patashu@hotmail.com>
2024-03-04 10:37:06 +1100

    Foot Wiggle de-cheese split, retire Graduation [VAR1], b15

--- GameLogic.gd
+++ GameLogic.gd
@@ -1188,7 +1188,7 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("TheOneWayPitEx")
 	level_filenames.push_back("TheSpikePitEx2")
 	level_filenames.push_back("OneWayToBurn")
-	level_filenames.push_back("GraduationSpicy")
+	level_filenames.push_back("FootWiggleEx")
 	level_filenames.push_back("InvisibleBridgeEx")
 	level_filenames.push_back("InvisibleBridgeEx2")
 	level_filenames.push_back("TheOneWayPitEx2")
