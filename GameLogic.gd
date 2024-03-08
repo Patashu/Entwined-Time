3b2ce3213d076c32e2b6cb1c78d8f779869937ec
Timothy Stiles <Patashu@hotmail.com>
2024-03-08 18:42:49 +1100

    more Spacetime Oven back a lil

--- GameLogic.gd
+++ GameLogic.gd
@@ -1509,10 +1509,10 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("Friendship Paradox")
 	level_filenames.push_back("Kinematic Stability")
 	level_filenames.push_back("Hot Soup [VAR2]")
+	level_filenames.push_back("Spacetime Oven")
 	level_filenames.push_back("Out of Service")
 	level_filenames.push_back("Pittance")
 	level_filenames.push_back("Second Wind")
-	level_filenames.push_back("Spacetime Oven")
 	
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(level_filenames.size());
