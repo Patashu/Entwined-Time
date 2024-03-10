88db9b92896726b0452097cbb18f3adcd5e91af5
Timothy Stiles <Patashu@hotmail.com>
2024-03-10 20:41:08 +1100

    community chapter split

--- GameLogic.gd
+++ GameLogic.gd
@@ -1497,11 +1497,6 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("Small Miracle")
 	level_filenames.push_back("Hot Soup")
 	level_filenames.push_back("Wheelbarrow")
-	level_filenames.push_back("PezDispenser")
-	level_filenames.push_back("Rushing Rivers")
-	level_filenames.push_back("Hokey Pokey")
-	level_filenames.push_back("Donk")
-	level_filenames.push_back("DonkEx")
 	level_filenames.push_back("Soft Landing")
 	level_filenames.push_back("Pure Vertical")
 	level_filenames.push_back("Beyond Even Gravity")
@@ -1514,6 +1509,24 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("Out of Service")
 	level_filenames.push_back("Pittance")
 	level_filenames.push_back("Second Wind")
+	chapter_advanced_starting_levels.push_back(level_filenames.size());
+	chapter_advanced_unlock_requirements.push_back(0);
+	
+	chapter_names.push_back("Future Technology");
+	chapter_standard_starting_levels.push_back(level_filenames.size());
+	chapter_standard_unlock_requirements.push_back(min(24, level_filenames.size()));
+	chapter_skies.push_back(Color("#223C52"));
+	chapter_tracks.push_back(0);
+	level_filenames.push_back("Rushing Rivers")
+	level_filenames.push_back("PezDispenser")
+	level_filenames.push_back("Hokey Pokey")
+	level_filenames.push_back("Donk")
+	level_filenames.push_back("DonkEx")
+	level_filenames.push_back("Waterslide")
+	level_filenames.push_back("Wall of Force")
+	chapter_replacements[chapter_names.size() - 1] = "!B";
+	chapter_advanced_starting_levels.push_back(level_filenames.size());
+	chapter_advanced_unlock_requirements.push_back(0);
 	
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(level_filenames.size());
