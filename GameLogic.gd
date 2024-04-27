3e4e8ead9ec148f8a64aa21bbf48e6d49dd2614a
Timothy Stiles <Patashu@hotmail.com>
2024-04-27 21:31:02 +1000

    hide Mail-In Parasite until it syncs

--- GameLogic.gd
+++ GameLogic.gd
@@ -1524,7 +1524,7 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("Pittance")
 	level_filenames.push_back("Second Wind")
 	level_filenames.push_back("Ionic Capacitor")
-	level_filenames.push_back("Mail-In Parasite")
+	#level_filenames.push_back("Mail-In Parasite")
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(0);
 	
