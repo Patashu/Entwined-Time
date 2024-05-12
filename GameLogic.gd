fc93a175ea5b85bbcb48319bf999e99ffd8cb5b1
Timothy Stiles <Patashu@hotmail.com>
2024-05-12 20:57:13 +1000

    move Toggle Latch to Advanced

--- GameLogic.gd
+++ GameLogic.gd
@@ -1512,12 +1512,12 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("Permafrost")
 	level_filenames.push_back("Grate Access")
 	level_filenames.push_back("Sewer Crawl")
-	level_filenames.push_back("Toggle Latch")
 	level_filenames.push_back("Zombie Mode")
 	level_filenames.push_back("The Zombie Pit")
 	
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(8);
+	level_filenames.push_back("Toggle Latch")
 	level_filenames.push_back("Three-Grate Monte")
 	level_filenames.push_back("Undying Army (Type A)")
 	level_filenames.push_back("Undying Army (Type B)")
