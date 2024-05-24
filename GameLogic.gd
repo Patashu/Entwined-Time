cdfe07fb3779abec73b7e3d98dce1deb9912b998
Timothy Stiles <Patashu@hotmail.com>
2024-05-24 15:18:50 +1000

    var1s of things

--- GameLogic.gd
+++ GameLogic.gd
@@ -1831,6 +1831,7 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("Phantom Push [VAR1]")
 	level_filenames.push_back("Anonymous Delivery [VAR1]")
 	level_filenames.push_back("Ankh [VAR1]")
+	level_filenames.push_back("Quantum Entanglement [VAR1]")
 	
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(0);
