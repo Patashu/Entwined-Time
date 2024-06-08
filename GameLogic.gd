395c1ce9206183f742ef31996059463c443a88ca
Timothy Stiles <Patashu@hotmail.com>
2024-06-08 19:18:53 +1000

    another final spikes de-cheese (and firewalkers)

--- GameLogic.gd
+++ GameLogic.gd
@@ -1767,6 +1767,7 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("Curveball")
 	level_filenames.push_back("Superpush")
 	level_filenames.push_back("Tiny Outpost")
+	level_filenames.push_back("Firewalkers")
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(0);
 	
