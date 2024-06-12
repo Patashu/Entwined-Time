103c1c0569f272a4cca600c307d7a43640085571
Timothy Stiles <Patashu@hotmail.com>
2024-06-12 10:39:24 +1000

    Circle Dance and Consecutive Normal Pits de-cheese

--- GameLogic.gd
+++ GameLogic.gd
@@ -1788,6 +1788,7 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("Firewalkers")
 	level_filenames.push_back("Fuzzy Patch")
 	level_filenames.push_back("Portal Repairing Crew (Insight)")
+	level_filenames.push_back("Circle Dance")
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(0);
 	
