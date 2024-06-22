110fc1a9aa30154ea53a324e7288beed6809f3b8
Timothy Stiles <Patashu@hotmail.com>
2024-06-22 10:56:53 +1000

    oops capitalization

--- GameLogic.gd
+++ GameLogic.gd
@@ -1945,7 +1945,7 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("One at a Time [VAR1]")
 	level_filenames.push_back("True Micro Puzzle")
 	level_filenames.push_back("True Micro Puzzle 2")
-	level_filenames.push_back("Cut and Paste")
+	level_filenames.push_back("Cut And Paste")
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(0);
 	
