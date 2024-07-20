a6e7274c49e08b0259354526ea814350cd8382bf
Timothy Stiles <Patashu@hotmail.com>
2024-07-20 21:07:26 +1000

    let's guard that

--- GameLogic.gd
+++ GameLogic.gd
@@ -2045,7 +2045,8 @@ func initialize_level_list() -> void:
 	chapter_standard_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 
-	#OS.set_clipboard(str(level_filenames));
+#	if (OS.is_debug_build()):
+#		OS.set_clipboard(str(level_filenames));
 
 	var current_standard_index = 0;
 	var current_advanced_index = 0;
