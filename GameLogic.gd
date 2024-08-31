d3fc92f3798802475ae1474fdd340d78729c3aa5
Timothy Stiles <Patashu@hotmail.com>
2024-08-31 16:17:45 +1000

    remove faulty level for now

--- GameLogic.gd
+++ GameLogic.gd
@@ -1900,7 +1900,6 @@ func initialize_level_list() -> void:
 	chapter_tracks.push_back(0);
 	chapter_replacements[chapter_names.size() - 1] = "CUSTOM";
 	level_filenames.push_back("Super Basic Sokoban")
-	level_filenames.push_back("Paper Planes [VAR1]  But Sandra Made Mistakes")
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(0);
 	
