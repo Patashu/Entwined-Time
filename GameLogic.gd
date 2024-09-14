3d132e1f83c7fee57201065ccf09cab6fb234978
Timothy Stiles <Patashu@hotmail.com>
2024-09-14 11:51:54 +1000

    ch3 shuffling

--- GameLogic.gd
+++ GameLogic.gd
@@ -1251,12 +1251,12 @@ func initialize_level_list() -> void:
 	level_filenames.push_back("EventHorizon")
 	level_filenames.push_back("PushingItSequel")
 	level_filenames.push_back("Daredevils")
-	level_filenames.push_back("SolidPuzzle")
 	level_filenames.push_back("FlowControl")
 	level_filenames.push_back("FootWiggle")
 	
 	chapter_advanced_starting_levels.push_back(level_filenames.size());
 	chapter_advanced_unlock_requirements.push_back(24);
+	level_filenames.push_back("SolidPuzzle")
 	level_filenames.push_back("RemoteVoyage")
 	level_filenames.push_back("SecurityDoorEx")
 	level_filenames.push_back("JailEx")
